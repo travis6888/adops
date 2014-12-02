5f03e3d02155d605f2c5815a92e5d49f71238ddf
Travis <travis6888@yahoo.com>
2014-12-01 17:32:23 -0800

    home page"

--- adops/urls.py
+++ adops/urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'adops.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^/$', 'optimize.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
