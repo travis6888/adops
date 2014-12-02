685b6aa4e05561bf9a65a364873e54bfa34a57a2
Travis <travis6888@yahoo.com>
2014-12-01 19:57:41 -0800

    duh

--- optimize/views.py
+++ optimize/views.py
@@ -1,6 +1,11 @@
+import csv
 from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
+    # with open(path) as f:
+    #     reader = csv.reader(f)
+    #     for row in reader:
     return render(request, 'home.html')
