22d5a0adf3444b62f163112572e4dab9a757786a
Travis <travis6888@yahoo.com>
2014-12-25 18:01:08 -0800

    pandas parse

--- optimize/views.py
+++ optimize/views.py
@@ -4,23 +4,37 @@ from sys import path
 from django.shortcuts import render
 
 # Create your views here.
+from pandas import ExcelFile, pandas
 import xlrd
 
 
 def home(request):
-    with open('optimize/adopstest.xls','rb') as f:
-        print xlrd.open_workbook(file_contents=mmap(f.fileno(),0,access=ACCESS_READ))
-        aString = open('optimize/adopstest.xls','rb').read()
-        workbook = xlrd.open_workbook('optimize/adopstest.xls')
-            # xlrd.open_workbook(file_contents=aString)
-        worksheet = workbook.sheet_by_name('Sheet1')
-        num_rows = worksheet.nrows-1
-        curr_row = -1
-        while curr_row < num_rows:
-            curr_row += 1
-            row = worksheet.row(curr_row)
-            print row
-            # print worksheet.row(34)
+    xl = pandas.ExcelFile('optimize/adopstest.xls')
+    # print xl.sheet_names
+
+    df = xl.parse("Sheet1", columns=list('abcdefgh'))
+    print df
+    # df["desired"] = df["10"] == 0
+    #     # if x == 0:
+    #     #     print "no"
+    #     # else:
+    #     #     print "yes"
+    # print df
+
+    # print df
+    # with open('optimize/adopstest.xls','rb') as f:
+    #     print xlrd.open_workbook(file_contents=mmap(f.fileno(),0,access=ACCESS_READ))
+    #     aString = open('optimize/adopstest.xls','rb').read()
+    #     workbook = xlrd.open_workbook('optimize/adopstest.xls')
+    #         # xlrd.open_workbook(file_contents=aString)
+    #     worksheet = workbook.sheet_by_name('Sheet1')
+    #     num_rows = worksheet.nrows-1
+    #     curr_row = -1
+    #     while curr_row < num_rows:
+    #         curr_row += 1
+    #         row = worksheet.row(curr_row)
+    #         print row
+    #         # print worksheet.row(34)
 
     # f = open('adcoloredreportdjango.csv', 'r')
     # for line in f:
