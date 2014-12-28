9d348dc33637b953d56e61b8b9d590653c47b26a
Travis <travis6888@yahoo.com>
2014-12-27 20:34:59 -0800

    parse and find coloumn values

--- optimize/views.py
+++ optimize/views.py
@@ -25,13 +25,16 @@ def home(request):
             row_types = worksheet.row_types(curr_row, 0, None)
             # print row_val[9]
             if row_val[10] > 0:
-                print "impressions", row_val[10], curr_row, row_val[16]
+                print "impressions", row_val[10], curr_row
                 if row_val[9] > 0:
                     print "yes clicks and impressions"
+                    if row_types[16] == 5:
+                        print "click, impressions but no su"
+                    else:
+                        print "su's {}".format(row_val[16]), row_val[10], curr_row
                 else:
-                    print " impressions and no clicks"
 
-
+                        print " impressions and no clicks"
             else:
                 if row_types[16] == 5:
                     print "this is error"
