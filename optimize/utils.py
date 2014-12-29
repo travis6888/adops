9398cadc0902f6d1187697d2d23040eafb5587ab
Travis <travis6888@yahoo.com>
2014-12-28 21:15:42 -0800

    upload file/ form fields

--- optimize/utils.py
+++ optimize/utils.py
@@ -1,4 +1,5 @@
 from fileinput import filename
+from mmap import mmap, ACCESS_READ
 from django.conf import settings
 import xlrd
 # from adops import settings
@@ -30,3 +31,36 @@ def handle_uploaded_file(f, name_file):
     with open(settings.MEDIA_ROOT + '{}.xls'.format(name_file), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+
+
+def open_file_sort():
+    with open('optimize/adopstest.xls','rb') as f:
+        print xlrd.open_workbook(file_contents=mmap(f.fileno(),0,access=ACCESS_READ))
+        aString = open('optimize/adopstest.xls','rb').read()
+        workbook = xlrd.open_workbook('optimize/adopstest.xls')
+            # xlrd.open_workbook(file_contents=aString)
+        worksheet = workbook.sheet_by_index(0)
+        num_rows = worksheet.nrows-1
+        curr_row = -1
+        while curr_row < num_rows:
+            row = worksheet.row(curr_row)
+            row_val = worksheet.row_values(curr_row, 0,None)
+            row_types = worksheet.row_types(curr_row, 0, None)
+            # print row_val[9]
+            if row_val[10] > 0:
+                print "impressions", row_val[10], curr_row
+                if row_val[9] > 0:
+                    print "yes clicks and impressions"
+                    if row_types[16] == 5:
+                        print "click, impressions but no su"
+                    else:
+                        print "su's {}".format(row_val[16]), row_val[10], curr_row
+                else:
+
+                        print " impressions and no clicks"
+            else:
+                if row_types[16] == 5:
+                    print "this is error"
+                else:
+                    print row_val[16]
+            curr_row += 1
--- optimize/views.py
+++ optimize/views.py
@@ -9,45 +9,12 @@ from django.shortcuts import render, redirect, render_to_response
 from pandas import ExcelFile, pandas
 import xlrd
 from optimize.forms import FileForm
-from optimize.utils import handle_uploaded_file
+from optimize.utils import handle_uploaded_file, open_file_sort
 
 
 def home(request):
     # print df
-    with open('optimize/adopstest.xls','rb') as f:
-        print xlrd.open_workbook(file_contents=mmap(f.fileno(),0,access=ACCESS_READ))
-        aString = open('optimize/adopstest.xls','rb').read()
-        workbook = xlrd.open_workbook('optimize/adopstest.xls')
-            # xlrd.open_workbook(file_contents=aString)
-        worksheet = workbook.sheet_by_index(0)
-        num_rows = worksheet.nrows-1
-        curr_row = -1
-        while curr_row < num_rows:
-            row = worksheet.row(curr_row)
-            row_val = worksheet.row_values(curr_row, 0,None)
-            row_types = worksheet.row_types(curr_row, 0, None)
-            # print row_val[9]
-            if row_val[10] > 0:
-                print "impressions", row_val[10], curr_row
-                if row_val[9] > 0:
-                    print "yes clicks and impressions"
-                    if row_types[16] == 5:
-                        print "click, impressions but no su"
-                    else:
-                        print "su's {}".format(row_val[16]), row_val[10], curr_row
-                else:
 
-                        print " impressions and no clicks"
-            else:
-                if row_types[16] == 5:
-                    print "this is error"
-                else:
-                    print row_val[16]
-                # print row_types[16]
-                # print cell(curr_row,row_val[16])
-                # print "no"
-                # print row_val[16]
-            curr_row += 1
 
     return render(request, 'home.html')
 
@@ -59,6 +26,7 @@ def file_upload(request):
             print "yes"
             handle_uploaded_file(request.FILES['file'], name_file=form.cleaned_data['name'])
             print form.cleaned_data['sheet_num']
+            open_file_sort()
             return HttpResponseRedirect('/home/')
     else:
 
